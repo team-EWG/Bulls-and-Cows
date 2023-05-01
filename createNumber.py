6bd7ed0753d46bda3b0057703d406c6270f0438b
kimchanho97 <nh0903@pusan.ac.kr>
2023-05-02 03:51:28 +0900

    feat: Done createNumber.py
    - complete createInputNum
    - inputNum is 4-digit number with no duplicate

--- createNumber.py
+++ createNumber.py
@@ -10,3 +10,12 @@ def createTargetNum():
     for i in range(4):
         target += (number[3-i] * 10 ** i)
     return target
+
+def createInputNum():
+    while True:
+        number = int(input("Insert number: "))
+        if number > 999:
+            break
+        else:
+            print("Number is invalid!")
+    return number
