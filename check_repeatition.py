2425c34e1a7dd3c9cfe8bfa76dd216218b23188d
Dongjin <ehdwls1638@gmail.com>
2023-05-03 00:59:53 +0900

    style : 필요없는 코드 삭제 #6

--- check_repeatition.py
+++ check_repeatition.py
@@ -1,12 +1,8 @@
 
 def is_usable_for_BAC(number_to_check):
-    if number_to_check < 100:
-        return False
-
     str_set_to_check = set(str(number_to_check))
     if number_to_check < 1000:
         str_set_to_check.add('0')
-    print(str_set_to_check)
 
     if len(str_set_to_check) == 4:
         return True
